f21c034689c87cfeeeca096f7579da0c2b076314
rex zhasm <rex@zhasm.com>
2010-12-10 13:42:58 +0800

    progressing

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 
-
 chrome.extension.sendRequest({greeting: "getVars"}, function(response) {
-  //console.log(response.k);
-  var x= document.createElement("input");
-  x.innerHTML = response.k;
-  //x.setAttribute("id","x_keyword");
-  //x.setAttribute("color",'yello');
-
-  document.body.appendChild(x);
+ 
+  var x= document.getElementById("miibeian");
+  x.setAttribute("k",response.k);
+  x.setAttribute("s",response.s);
+ 
 });
 
 
@@ -16,21 +13,173 @@ function addJQuery(callback) {
   script.setAttribute("src", "http://code.jquery.com/jquery-1.4.4.min.js");
   script.addEventListener('load', function() {
     var script = document.createElement("script");
-    script.textContent = "(" + callback.toString() + ")("");";
+    script.textContent = "(" + callback.toString() + ")();";
     document.body.appendChild(script);
   }, false);
   
   document.body.appendChild(script);
 }
 
-
-
 // the guts of this userscript
 function main(k) {
-    //alert(localStorage["favorite_color"]);
+    
+    var k=$("#miibeian").attr("k");
+    var s=$("#miibeian").attr("s");
+    
+    $("#miibeian").removeAttr("k");
+    $("#miibeian").removeAttr("s");
+    
+    function str2regex(s)
+    {
+        //'abc,xyz'=> /abc|xyz/i
+
+        var s=s.replace(/\s*,\s*/g, '|'); 
+        s=s.trim();
+        s=s.replace(/[\|,]+$/m, '');
+        if (!s)
+        {
+            return false;
+        }
         
-    //var g_keywords=readCookie("g_keywords");
-    //var g_sources=readCookie("g_sources");
+        s.replace("\\", "\\\\");
+        return RegExp(s);         
+    }
+
+    function hideItem(item)
+    {
+        //item is a child
+        var parent=item.parent();
+        var sibling=parent.children();
+        parent.click(function () {
+            var style=parent.attr('style');
+            if (style)
+            {
+                parent.attr("style","");    //reset style to default;
+                sibling.fadeIn('slow');     //show('slow'); 
+            }
+            else
+            {
+                sibling.hide();
+                parent.attr("style", "min-height:0px; height:5px"); 
+            }
+        });
+        sibling.hide();
+        parent.attr("style", "min-height:0px; height:5px"); 
+
+    }
+   
+    function filter(k, s)
+    {
+        //filter content that is matched regex k
+        //and source that is matched by regex s
+        if (s)
+        {
+            $(".method").each(function(){
+                var content=$(this).html();
+                var result=content.search(s);
+                result=parseInt(result);
+                if ( result != -1 ) //-1 means not found; != -1 means found
+                {
+                    hideItem($(this).parent());
+                }
+            });
+        }
+        
+       if (k)
+       {
+           $(".content").each(function(){
+               var content=$(this).html();
+               var result=content.search(k);
+               result=parseInt(result);
+               if ( result !=-1 ) // -1 means not found
+               {
+                   hideItem($(this));
+               }
+           });
+       }
+        
+    }//filter ends
+    
+    //repalce the search with advanced search:
+    $("li a[href='http://fanfou.com/search']").each(function(){
+        $(this).html("高级搜索"); 
+        $(this).attr("href", "http://fanfou.com/advancedsearch");
+    });
+
+    //get background image
+    //global function
+    var x=$(".vcard li:eq(1)").after('<li>背景：<a id="xbgimg" title="点击查看本页背景图" target="_blank" class="url" href="">点击查看</a>');
+    $("#xbgimg").click(function(){
+        var url=$("body").css("background-image");
+        url=url.replace(/^url|[('")]/gi,"");
+        window.open(url);
+        return false;
+    });
+
+//  page spacific function
+    var path=window.location.pathname; 
+
+    if (path.search("/home") !==-1){
+    
+
+        //when loading the page, filter:
+        var kr=str2regex(k);
+        var sr=str2regex(s);
+        
+        filter(kr, sr);
+        $("#pagination-more, #timeline-notification").click(function(){
+                setTimeout(function(){
+                    filter(kr, sr);
+                }, 3000);
+            filter(kr, sr);
+        });  
+
+    }
+
+    else if (path.search("advancedsearch") !==-1)
+    {
+        $("#pagination-more").click(function(){ 
+                var href=$(this).attr("href");
+                href=href.replace("/search","/advancedsearch");
+                window.location.href=href;
+        });
+
+        $(".act #keyword + .formbutton").click(function(){
+                var keyword=$("#keyword").val();
+                var url="http://fanfou.com/advancedsearch?q="+keyword; 
+                window.location.href=url;
+                return false; 
+        }); 
+    }
+    
+    //hyperlink
+    var fold;
+    var unfold;
+    var init=0;
+    $("#bio > a:visible").click(function(){
+        if (init==0)
+        {
+            fold=$("#bio").html();
+            setTimeout(function(){
+                unfold=$("#bio").html();
+                unfold=unfold.replace(/(?:ftp|https?):\/\/[^\s<>]+(?!["\s]+|<\/a>)/gi,"<a href='$&'>$&</a>");
+                //alert(unfold);
+                $("#bio").html(unfold);
+                //alert(unfold);
+            },100);
+            init=1;
+        }
+        else
+        {
+            $("#bio").html(unfold);
+        }
+        $('#user_infos > a ').click(function(
+            $("#bio").html(fold);
+            ));
+        
+    });//hyperlink end
+    
+   
     
 }
 
